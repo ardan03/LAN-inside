10ff58a0e908ecb1c137282a3d7ee3dcaf54d68c
Ardan Dordziev <ardandor120306@gail.com>
2024-03-15 20:43:42 +0800

    Передача через Json

---
+++ Client/Client/Configuration.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    public class Configuration
+    {
+        public string PcName {  get; set; }
+        public string osVersion { get; set;}
+        public string osBuild { get; set;}
+
+    }
+}
---
+++ serverTestXakaton1/serverTestXakaton1/Configuratin.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace serverTestXakaton1
+{
+    public class Configuratin
+    {
+        public string PcName { get; set; }
+        public string osVersion { get; set; }
+        public string osBuild { get; set; }
+    }
+}
