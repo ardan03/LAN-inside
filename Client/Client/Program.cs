fdccc42ca0a1b3f7504ff0a7943a974a7222bcd8
Ardan Dordziev <ardandor120306@gail.com>
2024-03-15 13:30:46 +0800

    Start project

---
+++ Client/Client/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Net.Sockets;
+using System.Text;
+using System.Management;
+
+using var tcpClient = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+try
+{
+    Console.WriteLine("Введите ip: ");
+    string ip = Console.ReadLine();
+    Console.WriteLine("Введите port: ");
+    int port = Convert.ToInt32(Console.ReadLine());
+
+    await tcpClient.ConnectAsync(ip, port);
+    // сообщение для отправки
+    var message = "s1ren КЛИЕНТ";
+    // считыванием строку в массив байт
+    byte[] requestData = Encoding.UTF8.GetBytes(message);
+    // отправляем данные
+    await tcpClient.SendAsync(requestData);
+    Console.WriteLine("Сообщение отправлено");
+    var searcher = new ManagementObjectSearcher("SELECT * FROM Win32_OperatingSystem");
+    foreach (ManagementObject os in searcher.Get())
+    {
+        string version = os["Caption"].ToString();
+        string buildNumber = os["BuildNumber"].ToString();
+        byte[] versionOS = Encoding.UTF8.GetBytes(version);
+        byte[] buildNum = Encoding.UTF8.GetBytes(buildNumber);
+        // отправляем данные
+        await tcpClient.SendAsync(versionOS);
+        await tcpClient.SendAsync(buildNum);
+
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
---
+++ serverTestXakaton1/serverTestXakaton1/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace serverTestXakaton1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+        int _countClient = 0;
+
+        async void BtnStat_Click(object sender, RoutedEventArgs e)
+        {
+            IPEndPoint ipPoint = new IPEndPoint(IPAddress.Any, 8888);
+            using Socket tcpListener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                tcpListener.Bind(ipPoint);
+                tcpListener.Listen();    // запускаем сервер
+                MessageBox.Show("Сервер запущен. Ожидание подключений... ");
+
+                while (true)
+                {
+                    // получаем подключение в виде TcpClient
+                    using var tcpClient = await tcpListener.AcceptAsync();
+                    // определяем буфер для получения данных
+                    byte[] responseData = new byte[512];
+                    int bytes = 0; // количество считанных байтов
+                    var response = new StringBuilder(); // для склеивания данных в строку
+                                                        // считываем данные 
+                    do
+                    {
+                        bytes = await tcpClient.ReceiveAsync(responseData);
+                        response.Append(Encoding.UTF8.GetString(responseData, 0, bytes));
+                    }
+                    while (bytes > 0);
+                    // выводим отправленные клиентом данные
+                   List.Items.Add(response.ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
